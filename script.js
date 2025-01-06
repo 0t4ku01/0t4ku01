372b02005124a3ccf7e31b26c953d4c4b8d715ce
0t4ku01 <iamt1cht@gmail.com>
2025-01-06 19:20:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Popup functionality
+function showPopup() {
+  document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+// Placeholder login/signup functions
+function login() {
+  alert('Login functionality coming soon!');
+}
+
+function signup() {
+  alert('Sign Up functionality coming soon!');
+}
